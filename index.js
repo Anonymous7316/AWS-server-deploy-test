163f92df21c484eaba30f70c5075ed3b92d30842
Dhawal Sarin <dhawal12sarin@gmail.com>
2025-03-09 12:26:50 +0530

    First commit.

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/Ping',(req,res)=>{
+    res.json({message:"Pong"});
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
+
